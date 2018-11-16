9c776963aded6112539fa925b2783890755674f7
AK33M <akeemtaiwo@outlook.com>
2018-11-16 15:12:48 +0000

    New hello component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
+import './index.css';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+import Hello from './Hello';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+  // <App />,
+  <Hello name="Sofia" enthusiasmLevel={5}/>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
